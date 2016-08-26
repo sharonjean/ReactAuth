7d82223fb88ce1e048557378118d4e9fc6492d6b
Sharon Claitor <sclaitor@gmail.com>
2016-08-25 21:50:27 -0700

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 class AppComponent extends Component {
 
   componentWillMount() {
-    this.lock = new Auth0Lock('YOUR_AUTH0_CLIENT_ID', 'YOUR_AUTH0_DOMAIN);
+    this.lock = new Auth0Lock('Gcd5vQWLGBINRkfM4GsKv6FBqdgHTpD9', 'sharonjean.auth0.com');
   }
 
   render() {
